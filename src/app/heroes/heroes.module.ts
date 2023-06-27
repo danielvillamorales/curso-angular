9631e8eaa230cdccf6e6b150dcd830d287d6cf23
daniel.villa <daniel.villa@utp.edu.co>
2023-06-27 14:11:21 -0500

    curso angular

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { ListComponent } from './components/list/list.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [HeroComponent,ListComponent,],
+  exports: [ListComponent,HeroComponent,],
+  imports: [CommonModule,],
+})
+export class HeroesModule {
+}
